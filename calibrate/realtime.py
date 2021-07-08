1c4c38d8dd3ca9c9fb831a334c2310428a062d8e
顏劭庭 <30778285+samuelyen36@users.noreply.github.com>
2021-07-08 14:57:47 +0800

    adding scatter histogram

--- calibrate/realtime.py
+++ calibrate/realtime.py
@@ -2,14 +2,16 @@ import cv2
 from time import sleep,time
 import os
 import numpy as np
-from utils import cal_reproject_error, dim_statistic, dim_stat_xy, sliding_window_calibrate, scatter_hist
+from utils import cal_reproject_error, sliding_window_calibrate, scatter_hist
 
-def capture(frame_count=20):
+def capture(frame_count=20, slide_threshold=10):        #frame_counter=> how many frames in total
     counter=0
     corner_x = 7   # pattern is 7*7
     corner_y = 7
     objp = np.zeros((corner_x*corner_y, 3), np.float32)
     objp[:, :2] = np.mgrid[0:corner_x, 0:corner_y].T.reshape(-1, 2)#[0 0 0],[1 0 0],[2 0 0]........[6 6 0]
+    _width=0
+    _height=0
 
     # Arrays to store object points and image points from all the images.
     objpoints = [] # 3d points in real world space
@@ -19,13 +21,13 @@ def capture(frame_count=20):
 
     start_time = time()
     print("a frame will be captured in three seconds")
-    while True:
+    while True:         #using infinite loop with timer to do the realtime capture and calibrate
         cur_time = time()
         ret, frame = cap.read()
         cv2.imshow('frame', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'): # 若按下 q 鍵則離開迴圈
             break
-        if cur_time - start_time > 3:
+        if cur_time-start_time > 3:
             if ret: #capture success
                 gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
                 ret, corners = cv2.findChessboardCorners(gray, (corner_x, corner_y), None)
@@ -36,34 +38,32 @@ def capture(frame_count=20):
                     #corners_with_three_d = np.c_[corners , np.zeros((49, 1, 1))]  # append zero to last dimension
                     imgpoints.append(corners)  
                     #above part for finding chessboard
-                    img_size = (frame.shape[1], frame.shape[0])
+                    _width=frame.shape[1]
+                    _height=frame.shape[0]
+                    img_size = (_width, _height)
 
-                    if counter>10:  #choosing when to do the sliding window
+                    if counter>slide_threshold:  #choosing when to do the sliding window
                         ret, mtx, dist, rvecs, tvecs, imgpoints, objpoints, err = sliding_window_calibrate(objpoints, imgpoints, img_size, counter, frame_count)
                     else:
                         ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, img_size, None, None)
                         err = cal_reproject_error(imgpoints,objpoints,rvecs,tvecs,mtx,dist)
 
                     print("error:{}".format(err))
-                    #if counter>1:
-                        #dim_statistic(imgpoints, frame.shape[1], frame.shape[0])
-                    #    dim_stat_xy(imgpoints, frame.shape[1], frame.shape[0])
                 else:
                     print("No chessboard is found in this frame")
+
                 print('\n')
-                if counter == frame_count:  #meet the number of frames defined before
+                if counter == frame_count:  #meet the number of frames defined in the begining
                     cap.release()
                     cv2.destroyAllWindows()
                     break
             start_time=cur_time
             print("a frame will be captured in three seconds")
     cap.release()
+    cv2.destroyAllWindows()
+    scatter_hist(imgpoints, _width, _height)
 
 
 if __name__ == '__main__':
   capture()
-  cv2.destroyAllWindows()
-
-
-  # 關閉所有 OpenCV 視窗
   #cv2.destroyAllWindows()
--- calibrate/utils.py
+++ calibrate/utils.py
@@ -4,7 +4,7 @@ from math import ceil
 import matplotlib.pyplot as plt
 
 
-def cal_reproject_error(imgpoints,objpoints,rvecs,tvecs,mtx,dist):  #calculate the reprojection error
+def cal_reproject_error(imgpoints, objpoints, rvecs, tvecs, mtx, dist):  #calculate the reprojection error
     mean_error = 0
     for i in range(len(objpoints)):
         imgpoints2, _ = cv2.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
@@ -19,15 +19,15 @@ def cal_reproject_error(imgpoints,objpoints,rvecs,tvecs,mtx,dist):  #calculate t
 34
 """
 def parse_imgpoints(imgpoints):     #sub_function used by dimension statistic
-    pts=np.array(imgpoints).squeeze(axis=None)
-    a,b,_ = pts.shape
+    pts = np.array(imgpoints).squeeze(axis=None)
+    a, b, _ = pts.shape
     pts = np.resize(pts,(a*b,2)).tolist()    #resize to the format we want
     return pts
 
 def dim_statistic(imgpoints,img_width,img_height):  #count the points with respect to four dimension
     half_width,half_height = img_height/2, img_height/2
     pts = parse_imgpoints(imgpoints)
-    dim_list=np.zeros(shape=4,dtype=np.int8).tolist()   #divide into four dimension
+    dim_list = np.zeros(shape=4, dtype=np.int8).tolist()   #divide into four dimension
     for item in pts:
         x,y = item
         if x > half_width and y > half_height:  #dim4
@@ -38,16 +38,16 @@ def dim_statistic(imgpoints,img_width,img_height):  #count the points with respe
             dim_list[1] += 1
         elif x > half_width and y < half_height: #dim1
             dim_list[0] += 1
-    _str='左上:{left_top}, 右上:{right_top}, 左下:{left_bottom}, 右下:{right_bottom}'.format(left_top=dim_list[1], right_top=dim_list[0],left_bottom=dim_list[2],right_bottom=dim_list[3])
+    _str = '左上:{left_top}, 右上:{right_top}, 左下:{left_bottom}, 右下:{right_bottom}'.format(left_top=dim_list[1], right_top=dim_list[0],left_bottom=dim_list[2],right_bottom=dim_list[3])
     print(_str) 
     return 
 
-def dim_stat_xy(imgpoints,img_width,img_height,div_num=10):    #project the points into x and y axis and do the further counting. Counting the distribution among x and y axis
+def dim_stat_xy(imgpoints, img_width, img_height, div_num=10):    #project the points into x and y axis and do the further counting. Counting the distribution among x and y axis
     pts = parse_imgpoints(imgpoints)
-    x_list , y_list = np.zeros(shape=div_num,dtype=np.int8).tolist() , np.zeros(shape=div_num,dtype=np.int8).tolist()
-    width_per_div , height_per_div = ceil(img_width/div_num) , ceil(img_height/div_num)
+    x_list , y_list = np.zeros(shape=div_num, dtype=np.int8).tolist() , np.zeros(shape=div_num, dtype=np.int8).tolist()
+    width_per_div, height_per_div = ceil(img_width/div_num) , ceil(img_height/div_num)
     for item in pts:
-        x,y = item
+        x, y = item
         x_list[int(x/width_per_div)] += 1
         y_list[int(y/height_per_div)] += 1  
     print("x axis(left to right):{}".format(str(x_list)))
@@ -55,27 +55,29 @@ def dim_stat_xy(imgpoints,img_width,img_height,div_num=10):    #project the poin
     return
 
 
-def sliding_window_calibrate(objpoints,imgpoints,img_size,cur_count,total): #if the newest data helps better performance, then discard the first. On the contary
-    packed_tmp = ret_tmp, mtx_tmp, dist_tmp, rvecs_tmp, tvecs_tmp = cv2.calibrateCamera(objpoints[:], imgpoints[:], img_size, None, None)  #using 1~last as new dataset, eliminate the first entry
-    packed_ori = ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints[:len(objpoints)-1], imgpoints[:len(imgpoints)-1], img_size, None, None)   #using 0~last-1
+def sliding_window_calibrate(objpoints, imgpoints, img_size, cur_count, total, eliminate=False): #if the newest data helps better performance, then discard the first. On the contary
+    packed_tmp = ret_tmp, mtx_tmp, dist_tmp, rvecs_tmp, tvecs_tmp = cv2.calibrateCamera(objpoints[:], imgpoints[:], #using 1~last as new dataset, eliminate the first entry
+                                                                                         img_size, None, None)  
+    packed_ori = ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints[:len(objpoints)-1], imgpoints[:len(imgpoints)-1], #using 0~last-1
+                                                                     img_size, None, None)   
     ori = cal_reproject_error(imgpoints[:len(imgpoints)-1],objpoints[:len(objpoints)-1],rvecs,tvecs,mtx,dist)
     slided = cal_reproject_error(imgpoints[:],objpoints[:],rvecs_tmp,tvecs_tmp,mtx_tmp,dist_tmp)
 
     if ori < slided:
         print('new data doesn\'t help, eliminate it')
-        return packed_ori + (imgpoints[:len(imgpoints)-1], objpoints[:len(objpoints)-1],ori)
+        return packed_ori + (imgpoints[:len(imgpoints)-1], objpoints[:len(objpoints)-1], ori)
     else:
         print("new data helps, add it to dataset")
-        return packed_tmp + (imgpoints[:], objpoints[:],slided)
+        return packed_tmp + (imgpoints[:], objpoints[:], slided)
     #return ret, mtx, dist, rvecs, tvecs
 
 
-def scatter_hist(imgpoints,_width,_height): #draw the scatter graph using imgpoints
+def scatter_hist(imgpoints, _width, _height, inverse=True): #draw the scatter graph using imgpoints
     pt = parse_imgpoints(imgpoints)
     x=[]
     y=[]
     for item in pt:
-        _x,_y = item
+        _x, _y = item
         x.append(_x)
         y.append(_y)
 
@@ -85,8 +87,8 @@ def scatter_hist(imgpoints,_width,_height): #draw the scatter graph using imgpoi
 
 
     rect_scatter = [left, bottom, width, height]
-    rect_histx = [left, bottom + height + spacing, width, 0.2]
-    rect_histy = [left + width + spacing, bottom, 0.2, height]
+    rect_histx = [left, bottom+height+spacing, width, 0.2]
+    rect_histy = [left+width+spacing, bottom, 0.2, height]
 
     # start with a square Figure
     fig = plt.figure(figsize=(8, 8))
@@ -100,13 +102,15 @@ def scatter_hist(imgpoints,_width,_height): #draw the scatter graph using imgpoi
 
     ax.set_xlim(0,_width)
     ax.set_ylim(0,_height)
+    if inverse:     #due to the different coordinate between images and 2-D coordinate(mainly on Y axis), we have to inverse y axis to get the correct result
+        ax.invert_yaxis()
     # the scatter plot:
     ax.scatter(x, y)
 
     # now determine nice limits by hand:
     binwidth = 0.25
     xymax = max(np.max(np.abs(x)), np.max(np.abs(y)))
-    lim = (int(xymax/binwidth) + 1) * binwidth
+    lim = (int(xymax/binwidth)+1) * binwidth
 
     bins = np.arange(-lim, lim + binwidth, binwidth)
     ax_histx.hist(x, bins=bins)
