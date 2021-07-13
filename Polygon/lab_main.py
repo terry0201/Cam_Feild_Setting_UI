6b68e7d25637ef32d22957f3a46436d230a65857
Meng-Xue, Lin <oscar19971003@gmail.com>
2021-07-13 17:59:36 +0800

    Update: 2021/07/13
    完成大部分功能
    
    TODO:
    1. 嘗試將 transformation 視窗調整為可伸縮/拖曳
    2. 與 Camera 功能連接

--- Polygon/lab_main.py
+++ Polygon/lab_main.py
@@ -1,13 +1,18 @@
 import sys
-from PySide2.QtWidgets import QApplication, QMainWindow
-from PySide2.QtCore import QFile
-from lab_UI import Ui_MainWindow
+from PySide2.QtWidgets import QApplication, QMainWindow, QWidget
+from PySide2.QtCore import QFile, Qt
+from lab_UI2 import Ui_MainWindow
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+
+        # 強制最大化
+        self.showMaximized()
+        # self.setFixedSize(1920, 1280)
+        self.setWindowFlags(Qt.MSWindowsFixedSizeDialogHint)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
@@ -16,5 +21,4 @@ if __name__ == "__main__":
     window.show()
     sys.exit(app.exec_())
 
-# 刪除多邊形方法: 點擊後滑鼠右鍵 -> 刪除
-# Open -> Ctrl + o
+# [-1, -1], [1, -1], [1, 1], [-1, 1]
